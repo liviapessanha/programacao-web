470a1f06fe4da1bbf55be4733d484f0282a9c76d
Lívia Pessanha <105252356+liviapessanha@users.noreply.github.com>
2025-02-10 21:51:16 -0300

    Add files via upload

---
+++ sgvet/src/main/java/br/edu/iff/ccc/bsi/sgvet/SgvetApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iff.ccc.bsi.sgvet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgvetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgvetApplication.class, args);
+	}
+
+}
---
+++ sgvet/src/test/java/br/edu/iff/ccc/bsi/sgvet/SgvetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iff.ccc.bsi.sgvet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgvetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
