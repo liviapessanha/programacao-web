d88396ef2073d859643f1803963c0d621c0eddeb
Livia Pessanha <liviapessanha99@gmail.com>
2025-05-08 08:38:47 -0300

    feat: modal para cadastro de cliente

--- sgvet/src/main/java/br/edu/iff/ccc/bsi/sgvet/controller/view/MainViewController.java
+++ sgvet/src/main/java/br/edu/iff/ccc/bsi/sgvet/controller/view/MainViewController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.edu.iff.ccc.bsi.sgvet.entities.Cliente;
+import br.edu.iff.ccc.bsi.sgvet.enums.Papel;
 import br.edu.iff.ccc.bsi.sgvet.repository.ClienteRepository;
 import jakarta.validation.Valid;
 
@@ -28,18 +29,27 @@ public class MainViewController {
 	
 	@GetMapping("clientes")
 	public String getClientes(Model model) {
-		Cliente  cliente = new Cliente();
-		model.addAttribute("cliente", cliente);
-		return "/clientes/clientes";
+		model.addAttribute("cliente", new Cliente());
+		model.addAttribute("clientes", clienteRep.findAll());
+		return "clientes/clientes";
 	}
 	
 	@PostMapping("/clientes")
 	 public String saveTask(@Valid @ModelAttribute("cliente") Cliente cliente, BindingResult result, Model model) { 
+		if(cliente.getPapel() == null) {
+			cliente.setPapel(Papel.CLIENTE);
+		}
+	
+		
 		if (result.hasErrors()) { 
-			model.addAttribute("cliente", cliente);
+			System.out.println("Tem erro!");
+			result.getAllErrors().forEach(System.out::println);
+			model.addAttribute("clientes", clienteRep.findAll());
 			return "/clientes/clientes"; // Retorna ao formulário se houver erros } 
 		}
+		
 			clienteRep.save(cliente); 
+			System.out.println("Salvo com sucesso!");
 			return "redirect:/clientes"; 
 		}
 	
