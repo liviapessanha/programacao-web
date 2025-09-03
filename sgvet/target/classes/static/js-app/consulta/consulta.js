193fd3d0fcd673044b7206578e089adbcaa40c97
Livia Pessanha <liviapessanha99@gmail.com>
2025-09-03 18:23:17 -0300

    add gitinore

--- sgvet/target/classes/static/js-app/consulta/consulta.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll(".edit-btn").forEach(button => {
-    button.addEventListener("click", function () {
-        const id = this.getAttribute("data-id");
-		
-        window.location.href = `/consultas/editar?id=${id}`;
-    });
-});
